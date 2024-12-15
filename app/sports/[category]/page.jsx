6c603d6fccbce3ada5ded09d129fef775c58a66c
Shalvin <shalvinshaji283@gmail.com>
2024-12-15 14:13:47 +0530

    products page dynamic routing - completed

---
+++ app/sports/[category]/page.jsx
@@ -0,0 +1,35 @@
+import { sportsData } from "@/lib/data";
+import ProductCard from "@/components/ProductCard";
+import PageHeading from "@/components/PageHeading";
+import Button from "@/components/Button";
+
+const page = ({ params }) => {
+  const { category } = params;
+  const categoryData = sportsData[category];
+
+  if (!categoryData) {
+    return <PageHeading pageTitle={"Page Not Found"} description={null} />;
+  }
+
+  const { pageHead, pageDesc, pageProducts } = categoryData;
+
+  return (
+    <div className="relative top-20  mb-24 py-5">
+      <Button />
+      <PageHeading pageTitle={pageHead} description={pageDesc} />
+      <div className="football_section grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        {pageProducts.map((product) => (
+          <ProductCard
+            key={product.id}
+            image={product.image}
+            price={product.price}
+            name={product.name}
+            // description={fb_data.description}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default page;
